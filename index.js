de4512a4028a50f3d4f2399949bff87c30ac2bbe
DIEGO <diego.a.galindo@gmail.com>
2020-03-20 15:52:13 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send({hi: "there"});
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+  console.log('server funning on port 3000');
+});
