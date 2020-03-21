a4bd3dca60b5c1c78f899dd5dd48dd9f2f87e57f
DIEGO <diego.a.galindo@gmail.com>
2020-03-21 02:05:14 -0500

    fix callback URI for google OAuth strategy

--- config/prod.js
+++ config/prod.js
@@ -5,10 +5,3 @@ module.exports={
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 }
-
-
-
-
-
-// prod google client id: 986475406125-bua2s045tev8q6b0qasus4fr73ci086s.apps.googleusercontent.com
-// prod google client secret: -YzGevII245EBP_6bRk1RJ9_
--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.GOOGLE_CLIENT_ID,
     clientSecret: keys.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
 
