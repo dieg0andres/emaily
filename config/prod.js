ec58566d3cc33ae7f0b3a8d4917d4240fe52f8c6
DIEGO <diego.a.galindo@gmail.com>
2020-03-21 01:47:32 -0500

    finish auth flow

---
+++ config/prod.js
@@ -0,0 +1,14 @@
+
+module.exports={
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+}
+
+
+
+
+
+// prod google client id: 986475406125-bua2s045tev8q6b0qasus4fr73ci086s.apps.googleusercontent.com
+// prod google client secret: -YzGevII245EBP_6bRk1RJ9_
---
+++ services/passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("User");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: keys.GOOGLE_CLIENT_ID,
+    clientSecret: keys.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+
+    User.findOne({ googleID:profile.id })
+      .then((existingUser) => {
+        if(existingUser) {
+          // we already have a record w/ given googleID
+          console.log('user already exists', existingUser)
+          cb(null, existingUser);
+
+        }
+        else {
+          // don't have a user record with googleID
+          new User({ googleID: profile.id }).save()
+            .then(user => cb(null, user));
+          console.log("new user created");
+        }
+      });
+  }
+));
